94de5be86b16ed1f647afec9f73e7c58a1b0d104
unknown <arscba@gmail.com>
2025-07-28 14:59:22 -0300

    User Update scenario added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,16 +7,34 @@ describe('Orange HRM Tests', () => {
         password: '[name="password"]',
         loginButton: '[type="submit"]',
         selectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-        wrongCredentialAlert: '[role="alert"]'
+        wrongCredentialAlert: '[role="alert"]',
+        myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+        firstNameField: '[name="firstName"]',
+        lastNameField: '[name="lastName"]',
+        generecField: '.oxd-input--active',
+        dateCloseButton: '.--close',
+        submitButton: '[type="submit"]'
+
     }
 
-    it('Login Success', () => {
+    it.only('Login Success', () => {
         cy.visit('/auth/login')
         cy.get(selectorsList.username).type(userData.userSuccess.username)
         cy.get(selectorsList.password).type(userData.userSuccess.password)
         cy.get(selectorsList.loginButton).click()
         cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
         cy.get(selectorsList.selectionTitleTopBar).contains('Dashboard')
+        cy.get(selectorsList.myInfoButton).click()
+        cy.get(selectorsList.firstNameField).type('FirstNameTeste')
+        cy.get(selectorsList.lastNameField).type('LastNameTeste')
+        cy.get(selectorsList.generecField).eq(2).type('Teste')
+        cy.get(selectorsList.generecField).eq(3).type('EmployeeTeste')
+        cy.get(selectorsList.generecField).eq(4).type('OtherTeste')
+        cy.get(selectorsList.generecField).eq(5).type('DriverTeste')
+        cy.get(selectorsList.generecField).eq(6).type('2025-04-12')
+        cy.get(selectorsList.dateCloseButton).click()
+        cy.get(selectorsList.submitButton).eq(1).click()
+        cy.get('.oxd-toast-content').should('contain', 'Successfully Saved')
     })
 
     it('Login Fail', () => {
